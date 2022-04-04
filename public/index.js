150358a2371818d6ccbb02fb110efe5517b53e3f
Eric Lowe <ericlowe25@gmail.ocm>
2022-04-04 19:02:21 -0400

    completed

--- public/index.js
+++ public/index.js
@@ -1,20 +1,49 @@
-function main(){
+async function main(){
     const countContainer = document.querySelector('#count-container');
+    const refreshButton = document.querySelector("#refresh-button")
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
 
-    let countValue = 0;
+    let response = await fetch("http://localhost:9001/counter");
+    let jsonData = await response.json();
+
+    let initCountValue = jsonData.value
+    let countValue = initCountValue;
+
+    setInterval(async ()=>{
+        let sync_response = await fetch("http://localhost:9001/counter");
+        countValue = (await sync_response.json()).value;
+        countContainer.textContent = countValue;
+    }, 200) //Set to 100-500ms
+
+
+    function refresh(){
+        countValue = initCountValue;
+        countContainer.textContent = countValue;
+        update()
+    }
+
+    async function update(){
+        await fetch("http://localhost:9001/counter", {
+            method:"PATCH",
+            headers: {'Content-Type': "application/json"},
+            body: JSON.stringify({'value': String(countValue)})
+        });
+    }
 
     function increment(){
         countValue++;
         countContainer.textContent = countValue;
+        update();
     }
 
     function decrement(){
         countValue--;
         countContainer.textContent = countValue;
+        update();
     }
 
+    refreshButton.addEventListener('click', refresh)
     incrementButton.addEventListener('click', increment);
     decrementButton.addEventListener('click', decrement);
     countContainer.textContent = countValue;
