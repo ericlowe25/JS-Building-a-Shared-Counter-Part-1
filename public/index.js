c4fc6481671c77173f3566c6776aa5bafa74bb44
Eric Lowe <ericlowe25@gmail.ocm>
2022-04-04 19:26:04 -0400

    completed

--- public/index.js
+++ public/index.js
@@ -1,51 +1,34 @@
 async function main(){
     const countContainer = document.querySelector('#count-container');
-    const refreshButton = document.querySelector("#refresh-button")
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
+    const refreshButton = document.querySelector('#refresh-button');
 
-    let response = await fetch("http://localhost:9001/counter");
-    let jsonData = await response.json();
+    const response = await fetch('http://localhost:9001/counter');
+    const result = await response.json();
 
-    let initCountValue = jsonData.value
-    let countValue = initCountValue;
-
-    setInterval(async ()=>{
-        let sync_response = await fetch("http://localhost:9001/counter");
-        countValue = (await sync_response.json()).value;
-        countContainer.textContent = countValue;
-    }, 200) //Set to 100-500ms
-
-
-    function refresh(){
-        countValue = initCountValue;
-        countContainer.textContent = countValue;
-        update()
-    }
-
-    async function update(){
-        await fetch("http://localhost:9001/counter", {
-            method:"PATCH",
-            headers: {'Content-Type': "application/json"},
-            body: JSON.stringify({'value': String(countValue)})
-        });
-    }
+    let countValue = result.value;
+    console.log(countValue);
 
     function increment(){
         countValue++;
         countContainer.textContent = countValue;
-        update();
     }
 
     function decrement(){
         countValue--;
         countContainer.textContent = countValue;
-        update();
     }
 
-    refreshButton.addEventListener('click', refresh)
+    function refresh(){
+        countValue = result.value;
+        countContainer.textContent = countValue;
+    }
+
     incrementButton.addEventListener('click', increment);
     decrementButton.addEventListener('click', decrement);
+    refreshButton.addEventListener('click', refresh);
+
     countContainer.textContent = countValue;
 }
 main()
